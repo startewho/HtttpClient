c4ecf8eb262dd50fb09ac46200f5dbeabe6917d8
chengpengfei <hoolooday@gmail.com>
2020-12-13 14:13:07 +0800

    添加说明

--- Main.cpp
+++ Main.cpp
@@ -24,6 +24,7 @@ void UploadIamge(string host,string file)
 
 	client.UploadForm(imageUrl, UploadInfo, Queries, response, httpCode);
 
+	client.CleanupSession();
 	string utf8Response = UTF8_To_GBK(response);
 
 	std::cout << utf8Response << std::endl;
@@ -54,6 +55,7 @@ void UploadProject(string host, string token,string file)
 	UploadInfo.AddFormFile("file", file);
 
 	client.UploadForm(imageUrl, UploadInfo, Queries, response, httpCode);
+	client.CleanupSession();
 
 	string utf8Response = UTF8_To_GBK(response);
 
@@ -64,8 +66,12 @@ void UploadProject(string host, string token,string file)
 int main()
 {
 	string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZGZzYXMiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIiLCJleHAiOjE2MDc4NTk4MDksImlzcyI6Ii9hcGkvdXNlci9sb2dpbiIsImF1ZCI6Ii9hcGkvdXNlci9sb2dpbiJ9.8nRCOeMAMeitGQJxfltm6y27fZxMIW7T_htQlE7ygFg";
+	std::cout << "http上传图片" << std::endl;
 	UploadIamge("http://localhost:5000/api","C:\\Users\\hoolo\\Pictures\\1.jpg");
+	std::cout << "http上传项目" << std::endl;
 	UploadProject("http://localhost:5000/api", token,"C:\\Users\\hoolo\\Pictures\\1.jpg");
+	std::cout << "https上传图片" << std::endl;
 	UploadIamge("https://localhost:443/api", "C:\\Users\\hoolo\\Pictures\\1.jpg");
+	std::cout << "https上传项目" << std::endl;
 	UploadProject("https://localhost:443/api", token, "C:\\Users\\hoolo\\Pictures\\1.jpg");
 }
